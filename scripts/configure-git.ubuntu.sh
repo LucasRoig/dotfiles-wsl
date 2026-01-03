f54f4ca56b23e4fad1221ee20439ffa8ff9faa00
Lucas Roig <lucas1.roig@gmail.com>
2026-01-03 17:22:55 +0100

    add git install

---
+++ scripts/configure-git.ubuntu.sh
@@ -0,0 +1,12 @@
+echo "Setting autocrlf to input"
+git config --global core.autocrlf input
+read -p "Enter Name: " fullname
+read -p "Enter Email: " email
+git config --global user.name "$fullname"
+git config --global user.email "$email"
+
+echo "Installing Git Credential Manager"
+git config --global credential.credentialStore plaintext
+curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+git-credential-manager configure
+rm -rf git-credential-manager
--- scripts/configure-kitty.ubuntu.sh
+++ scripts/configure-kitty.ubuntu.sh
@@ -3,6 +3,8 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # 2 - Desktop integration
 # Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in your system-wide PATH)
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/applications
 ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
 # Place the kitty.desktop file somewhere it can be found by the OS
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
